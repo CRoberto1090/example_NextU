b4ded74a716b6e7e034aeaea5cac4c8786b7a1ca
Carlos Roberto Cubero Campos <carlosrobertocc10@hotmail.com>
2016-08-02 18:38:53 -0600

    comentario

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Carlos Roberto Cubero Campos on 8/1/16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeMessage(sender: AnyObject) {
+        messageLabel.text = "Hola \(nameTextField.text)"
+        view.endEditing(true)
+    }
+    
+    // hola
+
+}
+
